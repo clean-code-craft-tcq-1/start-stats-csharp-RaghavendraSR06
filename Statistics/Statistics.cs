f46171db03c2a256a97cf10e6c480171e7936e13
Raghavendra S R (RBEI BSW3) <Raghavendra.SR2@in.bosch.com>
2021-01-27 16:31:40 +0530

    Functional method implementation for all test cases

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
-            //test checkin
+        Stats result = new Stats();
+
+        public Stats CalculateStatistics(List<double> numbers)
+        {
+            _ = numbers ?? throw new ArgumentNullException();
+            if (numbers.Any(doubleValue => Double.IsNaN(doubleValue)))
+            {
+                return null;
+            }
+            result.average = numbers.Average();
+            result.min = numbers.Min();
+            result.max = numbers.Max();
+            return result;
         }
+
     }
 }
---
+++ Statistics/StatsAlerter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Statistics
+{
+    public class StatsAlerter
+    {
+        public float MaxThreshold;
+        readonly EmailAlert _emailAlert;
+        readonly LEDAlert ledAlert;
+
+        public StatsAlerter(float maxThreshold, IAlerter[] alerters)
+        {
+            this.MaxThreshold = maxThreshold;
+            this._emailAlert = (Statistics.EmailAlert)alerters[0];
+            this.ledAlert = (Statistics.LEDAlert)alerters[1];
+        }
+
+        public void checkAndAlert(List<double> doubles)
+        {
+            foreach (var value in doubles.Where(value => value > MaxThreshold))
+            {
+                _emailAlert.emailSent = true;
+                ledAlert.ledGlows = true;
+            }
+        }
+    }
+
+    public class LEDAlert : IAlerter
+    {
+        public bool ledGlows = false;
+    }
+
+    public class EmailAlert : IAlerter
+    {
+        public bool emailSent = false;
+
+    }
+}
