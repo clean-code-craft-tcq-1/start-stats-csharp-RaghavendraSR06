b67b1bccf74f1f8db97f0219f63ba16bef529fa1
RaghavendraSR06 <raghu99459@gmail.com>
2021-01-28 21:51:14 +0530

    Update StatsUnitTest.cs
    changed the return type for stats class

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -12,23 +12,22 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var listValues = new List<double>{ 1.5, 8.9, 3.2, 4.5 };
-            statsComputer.CalculateStatistics(listValues);
+            var computedStats = statsComputer.CalculateStatistics(listValues);
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
 
         [Fact]
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
-            statsComputer.CalculateStatistics(
-                new List<double> { 1.5, 8.9, 3.2, 4.5, double.NaN });
-
-            Assert.True(double.IsNaN(statsComputer.average));
-            Assert.True(double.IsNaN(statsComputer.min));
-            Assert.True(double.IsNaN(statsComputer.max));
+            var list = new List<double> { 1.5, 8.9, 3.2, 4.5, double.NaN };
+            var computedStats =  statsComputer.CalculateStatistics(list);
+            Assert.True(double.IsNaN(computedStats.average));
+            Assert.True(double.IsNaN(computedStats.min));
+            Assert.True(double.IsNaN(computedStats.max));
         }
 
         [Fact]
