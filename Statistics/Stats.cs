3dd5d605860b858b09e172559533e4224b755fd2
RaghavendraSR06 <raghu99459@gmail.com>
2021-01-28 21:49:17 +0530

    Create Stats.cs
    Added Stats class

---
+++ Statistics/Stats.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistics
+{
+    public class Stats
+    {
+        public double average = double.NaN;
+        public double max = double.NaN;
+        public double min = double.NaN;
+    }
+}
